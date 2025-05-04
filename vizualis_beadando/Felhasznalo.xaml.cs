adb6a33426afb177108c5e808cc69e24447cd7b8
SiposVivien <siposvivien50@gmail.com>
2025-05-04 19:53:41 +0200

    orszagtortai_kesz

--- vizualis_beadando/Felhasznalo.xaml.cs
+++ vizualis_beadando/Felhasznalo.xaml.cs
@@ -11,7 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-using static Model.Felhasznalo;
+using Model;
+using vizualis_beadando.Data;
 
 namespace vizualis_beadando
 {
@@ -30,22 +31,10 @@ namespace vizualis_beadando
         {
             string username = txtUsername.Text;
 
-            if (string.IsNullOrEmpty(username))
+            using (var context = new AppDbContext())
             {
-                MessageBox.Show("Kérjük töltse ki a mezőt!");
-                return;
-            }
-
-            try
-            {
-                using (var context = new AppDbContext())
-                {
-                    var user = context.Felhasznalok.FirstOrDefault(u => u.felhasznalo_n == username);
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Hiba történt: {ex.Message}");
+                //var user = context.Felhasznalok
+                   // .FirstOrDefault(u => u.felhasznalo_n == username);
             }
         }
 
@@ -64,22 +53,25 @@ namespace vizualis_beadando
                 using (var context = new AppDbContext())
                 {
                     // Ellenőrizzük, hogy létezik-e már a felhasználó
-                    if (context.Felhasznalok.Any(u => u.felhasznalo_n == username))
+                    //if (context.Felhasznalok.Any(u => u.felhasznalo_n == username))
                     {
                         MessageBox.Show("Ez a felhasználónév már foglalt!");
                         return;
                     }
 
                     // Új felhasználó létrehozása
+
                     var newFelhasznalo = new Model.Felhasznalo
                     {
                         felhasznalo_n = username,
                     };
 
-                    context.Felhasznalok.Add(newFelhasznalo);
+                   // context.Felhasznalok.Add(newFelhasznalo);
                     context.SaveChanges();
+                    
 
                     MessageBox.Show("Sikeres regisztráció! elkezdheti a barangolást csodást receptjeink között.");
+                   
                 }
             }
             catch (Exception ex)
